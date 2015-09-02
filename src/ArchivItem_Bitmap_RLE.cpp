3f71ac9244ee03497a4a57ffdd07986937ffbf5d
Alexander Grund <Alex@grundis.de>
2015-09-02 19:18:52 +0200

    Remove svn headers

--- src/ArchivItem_Bitmap_RLE.cpp
+++ src/ArchivItem_Bitmap_RLE.cpp
@@ -1,5 +1,3 @@
-// $Id: ArchivItem_Bitmap_RLE.cpp 9359 2014-04-25 15:37:22Z FloSoft $
-//
 // Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
