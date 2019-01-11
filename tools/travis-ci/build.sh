2f8b8f37d7e94f6306b5b74316c2236a49a99e14
Flow86 <github@fd.mytrap.de>
2019-01-11 11:17:38 +0100

    BLD: clean up .travis.yml (use boost ppa and trusty lcov which has version 1.10)

--- tools/travis-ci/build.sh
+++ tools/travis-ci/build.sh
@@ -6,11 +6,19 @@ BUILD_TYPE=${1}
 CMAKE_FLAGS=${2}
 
 mkdir build && cd build
-cmake -DRTTR_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE} --generator="Unix Makefiles" ${CMAKE_FLAGS} ../examples
+cmake \
+    --generator="Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+    -DRTTR_ENABLE_WERROR=ON \
+    ${CMAKE_FLAGS} \
+    ../examples
+
 # Travis uses 2 cores
 make -j2
+
 # Execute tests
 export CTEST_OUTPUT_ON_FAILURE=1
 export BOOST_TEST_CATCH_SYSTEM_ERRORS="no"
 export RTTR_DISABLE_ASSERT_BREAKPOINT=1
+
 make test
