bad259adc9af7cca28a46a12f467270e98101f7a
Alexander Grund <Flamefire@users.noreply.github.com>
2021-07-08 18:35:20 +0200

    Add missing ci file

---
+++ tools/checkUTF8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright (C) 2005 - 2021 Settlers Freaks <sf-team at siedler25.org>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set -euo pipefail
+
+# Try to convert all source files to UTF-8 to detect encoding errors
+# Execute in the project folder (s25client)
+find . -type f \( -name '*.cpp' -o -name '*.hpp' -o -name '*.tpp' -o -name '*.h' \) -exec iconv -f UTF-8 > /dev/null {} \;
